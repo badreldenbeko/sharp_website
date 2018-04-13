efaf1002f4221e99e8bf007812acaa38c49a1e71
Badrelden Ahmed <badrelden@hotmail.com>
2018-04-13 16:26:24 +0200

    initial commit.
    Signed-off-by: Badrelden Ahmed <badrelden@hotmail.com>

--- manage.py
+++ manage.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharp.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-||||||| merged common ancestors
-=======
 #!/usr/bin/env python
 import os
 import sys
@@ -31,4 +13,3 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
->>>>>>> initial commit.
